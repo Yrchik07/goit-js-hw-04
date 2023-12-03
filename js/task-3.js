5b7097a478293c0ea65a492443b002c2cd784ccb
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2023-12-03 12:36:10 +0200

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,20 @@
 const profile = {
-    username: "Jacob",
+  username: "Jacob",
   playTime: 300,
+
+  changeUsername(newName) {
+      this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+      this.playTime += hours;
+  },
+
+  getInfo() {
+      return `${this.username} has ${this.playTime} active hours!`;
+  }
 };
 
-changeUsername(newName)
-updatePlayTime(hours)
-getInfo()  `<Username> has <amount> active hours!`
 
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
